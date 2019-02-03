09d11bd4919781db3b287f161323de96256c6d4a
Hunter Gregory <hlg16@duke.edu>
2019-02-02 23:02:38 -0500

    added method for creating a new grid from a parser based on a new xmlFile

---
+++ src/SimulatorMain.java
@@ -0,0 +1,29 @@
+import java.lang.reflect.Constructor;
+
+public class SimulatorMain {
+    public static final int GRID_DISPLAY_SIZE = 100;
+
+    private CA_TYPE myType;
+    private Grid myGrid;
+    private XMLParser myParser;
+
+
+    private void getNewGrid(String xmlFile) {
+        myParser.parseFile(xmlFile);
+        myType = myParser.getFileType();
+
+        int gridSize = myParser.getGridSize();
+        if (myParser.getIsRandom()) {
+            Constructor<Grid> constructor = myType.getRandomConstructor();
+            myGrid = constructor.newInstance(gridSize, GRID_DISPLAY_SIZE, myParser.getRandomMakeup());
+        }
+        else {
+            Constructor<Grid> constructor = myType.getConfiguredConstructor();
+            myGrid = constructor.newInstance(gridSize, GRID_DISPLAY_SIZE, myParser.getConfiguration());
+        }
+
+        Integer[] parameters = myParser.getParameters();
+        if (parameters.length > 0)
+            myGrid.setAdditionalParams(parameters);
+    }
+}
