6a96f8281c340800182c44a8c42a7803f1ab9029
Hunter Gregory <hlg16@duke.edu>
2019-02-03 01:00:37 -0500

    renamed directory, added warning in general CA, finished tester setup

--- src/XMLTestMain.java
+++ src/XMLTestMain.java
@@ -1,12 +1,18 @@
 
 public class XMLTestMain {
-    XMLParser myParser;
+    static XMLParser myParser;
 
     XMLTestMain() {
         myParser = new XMLParser();
     }
 
     public static void main(String[] args) {
-        myParser.parseFile();
+        myParser.parseFile("automata/fire-test-config.xml");
+        System.out.println(myParser.getCAType());
+        System.out.println(myParser.getConfiguration());
+        System.out.println(myParser.getGridSize());
+        System.out.println(myParser.getIsRandom());
+        System.out.println(myParser.getParameters());
+        System.out.println(myParser.getRandomMakeup());
     }
 }
