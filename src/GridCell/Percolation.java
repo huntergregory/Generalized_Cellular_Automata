5e38703530aa89895fa5bb3131d2baaf8c88f70f
Dhanush Madabusi <dm322@duke.edu>
2019-02-14 16:00:15 -0500

    fixed age and energy reset

--- src/GridCell/Percolation.java
+++ src/GridCell/Percolation.java
@@ -1,13 +1,41 @@
 package GridCell;
 
+import javafx.scene.paint.Color;
+
+import java.util.HashMap;
+
 /**
  *
  * @author Dhanush Madabusi
  */
-public abstract class Percolation extends Grid{
+public class Percolation extends Grid{
 
     public Percolation(int gridSize, double screenSize){
         super(gridSize, screenSize);
+    }
+
+    @Override
+    public void initStateColorMap() {
+        HashMap<Integer, Color> colorMap = new HashMap<Integer,Color>();
+        colorMap.put(0,Color.BLACK);
+        colorMap.put(1,Color.WHITE);
+        colorMap.put(2,Color.AQUAMARINE);
+        setStateColorMap(colorMap);
+    }
+
+    @Override
+    public void initSliderMap() {
+
+    }
+
+    @Override
+    public void setAdditionalParams(Double[] params) {
+
+    }
+
+    @Override
+    public void updateCells() {
+
     }
 
 }
